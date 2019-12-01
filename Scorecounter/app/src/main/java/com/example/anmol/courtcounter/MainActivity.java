97bf504725a7167f5cf86f40c739958cef646631
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2019-12-01 13:50:24 +0530

    2 Issues Completed Issue #2 and Issue #3 related to Volleyball Activity.

--- Scorecounter/app/src/main/java/com/example/anmol/courtcounter/MainActivity.java
+++ Scorecounter/app/src/main/java/com/example/anmol/courtcounter/MainActivity.java
@@ -7,20 +7,32 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.anmol.courtcounter.Basketball.BasketballActivity;
+import com.example.anmol.courtcounter.Volleyball.VolleyballActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button basketball;
+    Button volleyball;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         basketball = findViewById(R.id.button_basketball);
+        volleyball = findViewById(R.id.Volleyball);
+
+
         basketball.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, BasketballActivity.class));
+            }
+        });
+
+        volleyball.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, VolleyballActivity.class));
             }
         });
     }
---
+++ Scorecounter/app/src/main/java/com/example/anmol/courtcounter/Volleyball/VolleyballActivity.java
@@ -0,0 +1,15 @@
+package com.example.anmol.courtcounter.Volleyball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.anmol.courtcounter.R;
+
+public class VolleyballActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volleyball);
+    }
+}
