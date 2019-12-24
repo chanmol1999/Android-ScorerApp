ce97008032dc4cc656b82938cb00412362bda410
[name] <shashankshukla137@gmail.com>
2019-12-24 18:54:32 +0530

    about us activity added and also added the animation to the launch of the app.

--- Scorecounter/app/src/main/java/com/example/anmol/courtcounter/MainActivity.java
+++ Scorecounter/app/src/main/java/com/example/anmol/courtcounter/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.anmol.courtcounter;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,7 +15,8 @@ import com.example.anmol.courtcounter.Basketball.BasketballActivity;
 import com.example.anmol.courtcounter.TableTennis.tableTennisAcitivity;
 import com.example.anmol.courtcounter.Volleyball.VolleyballActivity;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
 
     Button basketball;
     Button volleyball;
@@ -25,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         basketball = findViewById(R.id.button_basketball);
         volleyball = findViewById(R.id.button_volleyball);
         badminton =  findViewById(R.id.button_badminton);
@@ -83,4 +92,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_about:
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
